07ed09b17cbab147fe52a1654f4c97c151a1bc78
moayd <m.o.aydogdu@outlook.com>
2024-01-23 02:01:29 +0300

    CREATE ProductPagingRequest.java

---
+++ src/main/java/org/example/simpleerp/model/dto/product/request/ProductPagingRequest.java
@@ -0,0 +1,12 @@
+package org.example.simpleerp.model.dto.product.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.example.simpleerp.common.model.dto.CustomPagingRequest;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ProductPagingRequest extends CustomPagingRequest {
+}
