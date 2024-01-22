07095d4e5d1ffdc0d98a17ecc664425dc4fd4bce
moayd <m.o.aydogdu@outlook.com>
2024-01-23 01:58:31 +0300

    CREATE CustomPaging.java

---
+++ src/main/java/org/example/simpleerp/common/model/CustomPaging.java
@@ -0,0 +1,25 @@
+package org.example.simpleerp.common.model;
+
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomPaging {
+
+    @Min(value = 1, message = "PageNumber alanı minimum 1 değerine sahip olmalıdır.")
+    private Integer pageNumber;
+
+    private Integer pageSize;
+
+    public Integer getPageNumber() {
+        return pageNumber - 1;
+    }
+
+}
