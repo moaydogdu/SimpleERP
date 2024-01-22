f309f955103c1dd429ebbbbe31d3fc28e6fb9091
moayd <m.o.aydogdu@outlook.com>
2024-01-23 01:59:51 +0300

    CREATE ProductCreateRequest.java

---
+++ src/main/java/org/example/simpleerp/model/dto/product/request/ProductCreateRequest.java
@@ -0,0 +1,24 @@
+package org.example.simpleerp.model.dto.product.request;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductCreateRequest {
+    @NotBlank
+    private String name;
+    @NotNull
+    private Long number;
+    @DecimalMin(value = "0.0001")
+    private BigDecimal price;
+}
