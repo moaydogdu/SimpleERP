0f75ba1438225eec10250f258187d7fdb8c731ba
moayd <m.o.aydogdu@outlook.com>
2024-01-23 04:36:09 +0300

    CREATE OrderCreateRequest.java

---
+++ src/main/java/org/example/simpleerp/model/dto/order/request/OrderCreateRequest.java
@@ -0,0 +1,23 @@
+package org.example.simpleerp.model.dto.order.request;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.example.simpleerp.model.dto.order.OrderProductRequest;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderCreateRequest {
+    @NotNull
+    private Long number;
+
+    @Valid
+    private List<OrderProductRequest> products;
+}
