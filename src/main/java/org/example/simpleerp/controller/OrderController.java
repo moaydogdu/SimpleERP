f50a1f10e49bcef2582592633231efdb16ff4da3
moayd <m.o.aydogdu@outlook.com>
2024-01-23 05:35:27 +0300

    REFACTOR OrderController.java -> Add getOrderById method.

--- src/main/java/org/example/simpleerp/controller/OrderController.java
+++ src/main/java/org/example/simpleerp/controller/OrderController.java
@@ -5,8 +5,12 @@ import lombok.RequiredArgsConstructor;
 import org.example.simpleerp.model.Order;
 import org.example.simpleerp.model.dto.order.request.OrderCreateRequest;
 import org.example.simpleerp.service.order.OrderCreateService;
+import org.example.simpleerp.service.order.OrderService;
+import org.hibernate.validator.constraints.UUID;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Validated
 public class OrderController {
 
+    private final OrderService orderService;
     private final OrderCreateService orderCreateService;
 
     // TODO : GET Order
@@ -33,6 +38,16 @@ public class OrderController {
                 .createOrder(orderCreateRequest);
 
         return ResponseEntity.ok(createdOrder);
+    }
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<Order> getOrderById(
+            @PathVariable("orderId") @UUID final String orderId
+    ) {
+        final Order orderFromDB = orderService
+                .getOrderById(orderId);
+
+        return ResponseEntity.ok(orderFromDB);
     }
 
 
