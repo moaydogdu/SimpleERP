1ea7fc73b798b61f12c5eac6c0b2af3dc7487430
moayd <m.o.aydogdu@outlook.com>
2024-01-23 05:50:09 +0300

    REFACTOR OrderServiceImpl.java -> Add getAllOrders method.

--- src/main/java/org/example/simpleerp/service/order/impl/OrderServiceImpl.java
+++ src/main/java/org/example/simpleerp/service/order/impl/OrderServiceImpl.java
@@ -1,13 +1,18 @@
 package org.example.simpleerp.service.order.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.example.simpleerp.common.model.CustomPage;
 import org.example.simpleerp.exception.order.OrderNotFoundException;
 import org.example.simpleerp.model.Order;
+import org.example.simpleerp.model.dto.order.request.OrderPagingRequest;
 import org.example.simpleerp.model.entity.OrderEntity;
 import org.example.simpleerp.model.mapper.order.OrderMapper;
 import org.example.simpleerp.repository.OrderRepository;
 import org.example.simpleerp.service.order.OrderService;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -24,5 +29,26 @@ public class OrderServiceImpl implements OrderService {
                 .orElseThrow(OrderNotFoundException::new);
 
         return OrderMapper.toDomainModel(orderEntityFromDB);
+    }
+
+    @Override
+    public CustomPage<Order> getAllOrders(
+            final OrderPagingRequest orderPagingRequest
+    ) {
+        final Page<OrderEntity> orderEntityPageFromDB = orderRepository
+                .findAll(orderPagingRequest.toPageable());
+
+        if (orderEntityPageFromDB.getContent().isEmpty())
+        {
+            throw new OrderNotFoundException();
+        }
+
+        final List<Order> orderDomainModels = OrderMapper
+                .toDomainModel(orderEntityPageFromDB.getContent());
+
+        return CustomPage.of(
+                orderDomainModels,
+                orderEntityPageFromDB
+        );
     }
 }
