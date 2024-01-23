779c8b2ba82ab429ceb9d3b37a133b73b06101ca
moayd <m.o.aydogdu@outlook.com>
2024-01-23 06:13:52 +0300

    CREATE OrderProductNotFoundException.java

---
+++ src/main/java/org/example/simpleerp/exception/orderProduct/OrderProductNotFoundException.java
@@ -0,0 +1,23 @@
+package org.example.simpleerp.exception.orderProduct;
+
+import java.io.Serial;
+
+public class OrderProductNotFoundException extends RuntimeException{
+
+    @Serial
+    private static final long serialVersionUID = -395457523408604559L;
+
+    private static final String DEFAULT_MESSAGE =
+            "OrderProduct Not Found";
+
+    public OrderProductNotFoundException(){
+        super(DEFAULT_MESSAGE);
+    }
+
+    public OrderProductNotFoundException(
+            final String message
+    ) {
+        super(DEFAULT_MESSAGE + " " + message);
+    }
+
+}
