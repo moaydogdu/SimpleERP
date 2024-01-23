a4bdec4297aa99fa40bc2b7653e65bf2ec8fc1a4
moayd <m.o.aydogdu@outlook.com>
2024-01-23 06:13:28 +0300

    CREATE OrderDeleteServiceImplOrderDeleteServiceImpl.java

---
+++ src/main/java/org/example/simpleerp/service/order/impl/OrderDeleteServiceImpl.java
@@ -0,0 +1,43 @@
+package org.example.simpleerp.service.order.impl;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.example.simpleerp.exception.order.OrderNotFoundException;
+import org.example.simpleerp.model.entity.OrderEntity;
+import org.example.simpleerp.repository.OrderRepository;
+import org.example.simpleerp.service.order.OrderDeleteService;
+import org.example.simpleerp.service.orderProduct.OrderProductDeleteService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderDeleteServiceImpl implements OrderDeleteService {
+
+    private final OrderRepository orderRepository;
+    private final OrderProductDeleteService orderProductDeleteService;
+
+    @Override
+    @Transactional
+    public void deleteOrderById(
+            final String orderId
+    ) {
+        final OrderEntity orderEntityFromDBToBeDelete = orderRepository
+                .findById(orderId)
+                .orElseThrow(OrderNotFoundException::new);
+
+        if (orderEntityFromDBToBeDelete.getOrderProductEntities().isEmpty()){
+            orderRepository.delete(orderEntityFromDBToBeDelete);
+        }
+
+        orderEntityFromDBToBeDelete.getOrderProductEntities()
+                .forEach(
+                        orderProductEntity -> orderProductDeleteService
+                                .deleteOrderProductById(
+                                        orderProductEntity.getId()
+                                )
+                );
+
+        orderRepository.delete(orderEntityFromDBToBeDelete);
+
+    }
+}
