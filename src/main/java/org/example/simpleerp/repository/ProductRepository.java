e2447d88f1f021d4a132e8ac7d0d97f0bfecec88
moayd <m.o.aydogdu@outlook.com>
2024-01-23 05:25:51 +0300

    REFACTOR ProductRepository.java -> Add findProductEntityByNumber method.

--- src/main/java/org/example/simpleerp/repository/ProductRepository.java
+++ src/main/java/org/example/simpleerp/repository/ProductRepository.java
@@ -4,9 +4,15 @@ import org.example.simpleerp.model.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, String> {
     boolean existsProductEntityByNumber(
+            final Long number
+    );
+
+    Optional<ProductEntity> findProductEntityByNumber(
             final Long number
     );
 }
