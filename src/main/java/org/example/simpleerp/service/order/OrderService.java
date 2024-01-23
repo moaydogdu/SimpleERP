14b0c6857289b727dfeb75526c416433b8094113
moayd <m.o.aydogdu@outlook.com>
2024-01-23 05:49:57 +0300

    REFACTOR OrderService.java -> Add getAllOrders method.

--- src/main/java/org/example/simpleerp/service/order/OrderService.java
+++ src/main/java/org/example/simpleerp/service/order/OrderService.java
@@ -1,9 +1,15 @@
 package org.example.simpleerp.service.order;
 
+import org.example.simpleerp.common.model.CustomPage;
 import org.example.simpleerp.model.Order;
+import org.example.simpleerp.model.dto.order.request.OrderPagingRequest;
 
 public interface OrderService {
     Order getOrderById(
             final String orderId
+    );
+
+    CustomPage<Order> getAllOrders(
+            final OrderPagingRequest orderPagingRequest
     );
 }
