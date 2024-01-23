d9e5b66c6ab3c84ab592fbecc43bb185cbc01c84
moayd <m.o.aydogdu@outlook.com>
2024-01-23 05:23:48 +0300

    CREATE OrderProductRequest.java

---
+++ src/main/java/org/example/simpleerp/model/dto/order/OrderProductRequest.java
@@ -0,0 +1,22 @@
+package org.example.simpleerp.model.dto.order;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderProductRequest {
+    @NotNull
+    private Long productNumber;
+    @DecimalMin(value = "0.0001")
+    private BigDecimal amount;
+    private BigDecimal unitPrice;
+}
