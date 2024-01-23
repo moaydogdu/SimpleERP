624c94b595c981922cd54403734087cd12b93277
moayd <m.o.aydogdu@outlook.com>
2024-01-23 05:50:43 +0300

    CREATE OrderPagingRequest.java

---
+++ src/main/java/org/example/simpleerp/model/dto/order/request/OrderPagingRequest.java
@@ -0,0 +1,12 @@
+package org.example.simpleerp.model.dto.order.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.example.simpleerp.common.model.dto.CustomPagingRequest;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class OrderPagingRequest extends CustomPagingRequest {
+}
