04c5444792e542f721801dd12b31429070153d2b
moayd <m.o.aydogdu@outlook.com>
2024-01-23 06:13:36 +0300

    CREATE OrderProductDeleteService.java

---
+++ src/main/java/org/example/simpleerp/service/orderProduct/OrderProductDeleteService.java
@@ -0,0 +1,7 @@
+package org.example.simpleerp.service.orderProduct;
+
+public interface OrderProductDeleteService {
+    void deleteOrderProductById(
+            final String orderProductId
+    );
+}
