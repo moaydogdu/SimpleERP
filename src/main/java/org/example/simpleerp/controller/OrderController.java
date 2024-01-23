3e8cc6cdaf11977826acef441f88140cf2999d02
moayd <m.o.aydogdu@outlook.com>
2024-01-23 05:24:34 +0300

    CREATE OrderController.java -> Add createOrder method.

--- src/main/java/org/example/simpleerp/controller/OrderController.java
+++ src/main/java/org/example/simpleerp/controller/OrderController.java
@@ -1,7 +1,14 @@
 package org.example.simpleerp.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.example.simpleerp.model.Order;
+import org.example.simpleerp.model.dto.order.request.OrderCreateRequest;
+import org.example.simpleerp.service.order.OrderCreateService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +17,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Validated
 public class OrderController {
+
+    private final OrderCreateService orderCreateService;
+
+    // TODO : GET Order
+    // TODO : GETALL Order
+    // TODO : UPDATE Order
+    // TODO : DELETE Order
+
+    @PostMapping
+    public ResponseEntity<Order> createOrder(
+            @RequestBody @Valid final OrderCreateRequest orderCreateRequest
+    ) {
+        final Order createdOrder = orderCreateService
+                .createOrder(orderCreateRequest);
+
+        return ResponseEntity.ok(createdOrder);
+    }
+
+
 }
