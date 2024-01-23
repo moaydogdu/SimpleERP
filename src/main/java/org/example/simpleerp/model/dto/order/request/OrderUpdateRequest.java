01a318bda2ba4d9abe163ed592632fac4c61e19a
moayd <m.o.aydogdu@outlook.com>
2024-01-23 06:14:00 +0300

    CREATE OrderUpdateRequest.java

---
+++ src/main/java/org/example/simpleerp/model/dto/order/request/OrderUpdateRequest.java
@@ -0,0 +1,16 @@
+package org.example.simpleerp.model.dto.order.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderUpdateRequest {
+    @NotNull
+    private Long number;
+}
