1418ccfe576e9480698fe46be0821471182e8082
moayd <m.o.aydogdu@outlook.com>
2024-01-24 03:52:09 +0300

    REFACTOR OrderRepository.java

--- src/main/java/org/example/simpleerp/repository/OrderRepository.java
+++ src/main/java/org/example/simpleerp/repository/OrderRepository.java
@@ -2,11 +2,17 @@ package org.example.simpleerp.repository;
 
 import org.example.simpleerp.model.entity.OrderEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
 
 @Repository
 public interface OrderRepository extends JpaRepository<OrderEntity, String> {
     boolean existsOrderEntityByNumber(
             final Long number
     );
+
+    @Query("SELECT COALESCE(SUM(o.totalPrice), 0) FROM OrderEntity o")
+    BigDecimal getTotalPriceSum();
 }
