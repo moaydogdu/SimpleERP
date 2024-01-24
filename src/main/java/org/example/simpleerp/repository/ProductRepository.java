1ae5394051cd82a18d6ac021f0017b7b92b13f44
moayd <m.o.aydogdu@outlook.com>
2024-01-24 03:52:00 +0300

    REFACTOR ProductRepository.java

--- src/main/java/org/example/simpleerp/repository/ProductRepository.java
+++ src/main/java/org/example/simpleerp/repository/ProductRepository.java
@@ -4,6 +4,7 @@ import org.example.simpleerp.model.entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,4 +16,6 @@ public interface ProductRepository extends JpaRepository<ProductEntity, String> 
     Optional<ProductEntity> findProductEntityByNumber(
             final Long number
     );
+
+    List<ProductEntity> findAllByOrderByNumberAsc();
 }
